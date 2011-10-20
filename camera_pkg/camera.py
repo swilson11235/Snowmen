60f713257a5d47f217f4be655ecf51f829ff89c4
Stephen Wilson <swilson11235@gmail.com>
2011-10-19 18:11:33 -0600

    working rotation

--- camera_pkg/camera.py
+++ camera_pkg/camera.py
@@ -1,5 +1,5 @@
 
-__author__ = 'Stephen'
+__author__ = 'Stephen and Will'
 __version__ = '1.0'
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
@@ -15,35 +15,19 @@ class camera():
         self.posx=0
         self.posy=0
         self.posz=3
-        self.factor=1
+        self.factor=10
+        self.tmp_y_rot=0
+        self.tmp_x_rot=0
+        self.tmp_z_rot=0
     def rendercamera(self):
         glRotatef(-self.x_rot , 1.0, 0.0, 0.0)
         glRotatef(-self.y_rot , 0.0, 1.0, 0.0)
         glRotatef(-self.z_rot , 0.0, 0.0, 1.0)
-        glTranslatef(0, 0, -self.posz )
-        glScale(self.factor,self.factor,self.factor)
-    def strafeleft(self):
-        print "Camera is at: ", self.posx, self.posy, self.posz
+        glTranslatef(-self.posx, -self.posy, -self.posz )
+    def strafeleft(self,num=10.0):
         self.posx-=.1
-        if self.posx<0:
-            self.factor-=.1
-        if self.posx>0:
-            self.factor+=.1
-        if (self.posz or self.posx)==0:
-            pass
-        else:
-            self.y_rot = math.tan(self.posz/self.posx)*180/math.pi
     def straferight(self):
-        print "Camera is at: ", self.posx, self.posy, self.posz
         self.posx+=.1
-        if self.posx>0:
-            self.factor-=.1
-        if self.posx<0:
-            self.factor+=.1
-        if (self.posz or self.posx)==0:
-            pass
-        else:
-            self.y_rot = math.tan(self.posz/self.posx)*180/math.pi
     def moveup(self):
         self.posy+=.1
     def movedown(self):
@@ -52,3 +36,22 @@ class camera():
         self.posz-=.1
     def walkout(self):
         self.posz+=.1
+    def rotate(self,num=10.0):
+        self.tmp_y_rot+=float(num)
+        if self.tmp_y_rot>360:
+            self.tmp_y_rot=0
+        self.posz = 3*math.cos(self.tmp_y_rot/180*math.pi)
+        if self.tmp_y_rot==90 or self.tmp_y_rot==270:
+            self.y_rot+=180
+
+        self.tmp_x_rot+=float(num)
+        if self.tmp_x_rot>360:
+            self.tmp_x_rot=0
+        self.posy = math.sin(self.tmp_x_rot/180*math.pi)
+        if self.tmp_x_rot==90 or self.tmp_x_rot==270:
+            self.factor*=-1
+        self.x_rot= self.x_rot-self.factor
+
+
+
+        
