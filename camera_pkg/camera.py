3ed6dc4bcc59afe437c67af276352ad6b68a6949
Stephen Wilson <swilson11235@gmail.com>
2011-10-20 11:50:37 -0600

    final

--- camera_pkg/camera.py
+++ camera_pkg/camera.py
@@ -1,12 +1,14 @@
 '''The camera class allows for manipulation of a theoretical camera in a OpenGl object.'''
+
 __author__ = 'Stephen and Will'
 __version__ = '1.0'
+
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 from OpenGL.GL import *            
 import math
 
-class camera():
+class camera:
     def __init__(self,):
         '''Initializes a camera.'''
         self.x_rot=0
@@ -19,7 +21,7 @@ class camera():
         self.tmp_y_rot=0
         self.tmp_x_rot=0
         self.tmp_z_rot=0
-       # self.current=0
+        self.current=0
     def rendercamera(self):
         '''Renders the camera.'''
         glRotatef(-self.x_rot , 1.0, 0.0, 0.0)
@@ -29,34 +31,34 @@ class camera():
     def strafeleft(self,num=10.0):
         '''Does a basic strafe left.'''
         self.posx-=.1
-        #self.current=0
+        self.current=0
     def straferight(self):
         '''Does a basic strafe right.'''
         self.posx+=.1
-        #self.current=0
+        self.current=0
     def moveup(self):
         '''Does a basic move up.'''
         self.posy+=.1
-       # self.current=0
+        self.current=0
     def movedown(self):
         '''Does a basic move down.'''
         self.posy-=.1
-       # self.current=0
+        self.current=0
     def walkin(self):
         '''Walks in properly.'''
         self.posz-=.1
-       # self.current=0
+        self.current=0
     def walkout(self):
         '''Walks out properly.'''
         self.posz+=.1
-       # self.current=0
+        self.current=0
     def rotate(self,num=10.0):
         '''Does a rotation around the x axis. Takes a num that relates to how fast in degrees to rotate.'''
-        # self.current+=1
-        # if self.current==1:
-        #     self.tmp_y=self.posy
-        #     self.tmp_x=self.posx
-        #     self.tmp_z=self.posz
+        self.current+=1
+        if self.current==1:
+            self.tmp_y=self.posy
+            self.tmp_x=self.posx
+            self.tmp_z=self.posz
         self.tmp_y_rot+=float(num)
         if self.tmp_y_rot>360:
             self.tmp_y_rot=0
@@ -66,11 +68,12 @@ class camera():
        
         self.tmp_x_rot+=float(num)
         if self.tmp_x_rot>360:
-           # glTranslatef(-self.tmp_x,self.tmp_y, self.tmp_z)
-            self.tmp_x_rot=0
+             glTranslatef(-self.tmp_x,-self.tmp_y, -self.tmp_z)
+             self.tmp_x_rot=0
         self.posy = math.sin(self.tmp_x_rot/180*math.pi)
         if self.tmp_x_rot==90 or self.tmp_x_rot==270:
             self.factor*=-1
         self.x_rot= self.x_rot-self.factor
+        print self.posx,self.posy,self.posz
 
         
